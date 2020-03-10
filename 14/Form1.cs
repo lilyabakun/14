7aefdafe8413dcf4501281e6c81d9adefa6888fa
lilyabakun <lilya.bakun23@gmail.com>
2020-03-10 19:57:39 +0300

    Добавьте файлы проекта.

---
+++ 14/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graph.Graph obj = new Graph.Graph();
+            float x = 0;
+            if (!Single.TryParse(textBox1.Text, out x))
+            {
+                MessageBox.Show("Строка содержит неправильное число", "Ошибка");
+                return;
+            }
+            double y = obj.Task(Convert.ToDouble(textBox1.Text));
+            if (y != double.NaN)
+            {
+                label2.Text = String.Format("y= {0:0.00}", Convert.ToDouble(y));
+            }
+            else { MessageBox.Show("X не принадлежит графику функции", "Ошибка");}
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { }
+    }
+}
